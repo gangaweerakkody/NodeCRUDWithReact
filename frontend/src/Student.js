34aa7cf215f4c7edf6487a94098088a92d53f469
gangaweerakkody <127628647+gangaweerakkody@users.noreply.github.com>
2025-01-20 23:07:42 +0530

    changes in frontend

--- frontend/src/Student.js
+++ frontend/src/Student.js
@@ -2,34 +2,37 @@
 
 function Student() {
   return (
-    <div className="container">
-        <div className="body">
-           <form method="" >
-            <div className="form-row">
-                <div className="input-data">
-                <input type="text" required/>
-                        <div classname="underline"> </div>
-                         <label for="">Name</label>
-                     </div>
-                     <div className="input-data">
-                     <input type="email"  required/>
-                        <div classname="underline">
-
-                        </div>
-                        <label for="">Email</label>
-                     </div>
-                 </div>
-                    <div className="submit-btn">
-                        <div className="input-data">
-                            <div className="inner"></div>
-                            <input type="submit" value="submit"/>
-                        </div>
-                    </div>
-    
-            
-           </form>
-        </div>
-        
+    <div class="container">
+    <div class="text">
+       Contact us Form
+    </div>
+    <form action="#">
+       <div class="form-row">
+          <div class="input-data">
+             <input type="text" required/>
+             <div class="underline"></div>
+             <label for="">Full Name</label>
+          </div>
+          
+       </div>
+       <div class="form-row">
+          <div class="input-data">
+             <input type="text" required/>
+             <div class="underline"></div>
+             <label for="">Email Address</label>
+          </div>
+          
+       </div>
+      
+         
+          <div class="form-row submit-btn">
+             <div class="input-data">
+                <div class="inner"></div>
+                <input type="submit" value="submit"/>
+             </div>
+          </div>
+          
+    </form>
     </div>
   )
 }
