290522a7f5c336b682055f03b5a08b4d0638058b
gangaweerakkody <127628647+gangaweerakkody@users.noreply.github.com>
2025-01-28 22:36:27 +0530

    created.css

--- frontend/src/Create.js
+++ frontend/src/Create.js
@@ -1,17 +1,20 @@
 import React from 'react'
-
+import './create.css'
 function Create() {
   return (
  <div className="container">
       <h2>Create Student</h2>
     <form action="#" >
     
-        <div className="Name">
+        <div className="name">
             <label htmlFor="name">Student Name</label>
             <input type="text" placeholder="Enter Name" className="form-control"/>
-
-        </div>
-
+         </div>
+         <div className='email'>
+            <label htmlFor="email">Email</label>
+            <input type="email" placeholder="Enter Email" className="form-control"/>
+         </div>
+        <button type="submit" className="submit">Submit</button>
     </form>
  </div>
   
