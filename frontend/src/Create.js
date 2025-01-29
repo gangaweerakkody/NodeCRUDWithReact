f41a9b474e3e3d7ca259fc66c458f8cbbcf1255c
gangaweerakkody <127628647+gangaweerakkody@users.noreply.github.com>
2025-01-29 22:42:05 +0530

    Update Create.js

--- frontend/src/Create.js
+++ frontend/src/Create.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React , {useState} from 'react'
 import './create.css'
+import { useNavigate } from 'react-router-dom'
 function Create() {
    const[name,setName]= useState('')
    const[email,setEmail]= useState('')
+   const navigate= useNavigate();
 
+function handleSubmit(event){
+   event.preventDefault();
+   axios.post('http://localhost:8082/create',{name,email})
+   .then(res => {
+      console.log(res);
+      navigate('/');
+
+   })  .catch(err => {console.log(err);})
+}
   return (
  <div className="container">
       <h2>Create Student</h2>
-    <form action="#" >
+    <form action="#"  onSubmit={handleSubmit}>
     
         <div className="name">
             <label htmlFor="name">Student Name</label>
@@ -15,7 +26,7 @@ function Create() {
          </div>
          <div className='email'>
             <label htmlFor="email">Email</label>
-            <input type="email" placeholder="Enter Email" className="form-control"/>
+            <input type="email" placeholder="Enter Email" className="form-control"onChange={e => setEmail(e.target.value)}/>
          </div>
         <button type="submit" className="submit">Submit</button>
     </form>
